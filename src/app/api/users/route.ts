a0d577fcb5b90b6b6fa66fc9d364d1ca2eeafd10
ribgu <gustavo.ribeiro@sptech.school>
2024-07-12 19:07:47 -0300

    cria rota de criar usuário

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,20 @@
+import createNewUser from '@/lib/services/user/create-new-user'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET() {
+  return NextResponse.json('Healthy')
+}
+
+export async function POST(request: NextRequest) {
+    const { name, email, image, role, document } = await request.json()
+
+    const user = await createNewUser({
+        name,
+        email,
+        image,
+        role,
+        document
+    })
+
+    return NextResponse.json(user)
+}
---
+++ src/lib/services/user/create-new-user.ts
@@ -0,0 +1,25 @@
+import { prisma } from '@/lib/prisma/prismaClient'
+
+type CreateUserParams = {
+    name?: string
+    email: string
+    image?: string
+    role: string
+    document ?: string
+}
+
+export default function createNewUser(User: CreateUserParams) {
+    const { name, email, image, role, document } = User
+
+    const newUser = prisma.user.create({
+        data: {
+            name,
+            email,
+            image,
+            role,
+            document
+        }
+    })
+
+    return newUser
+}
