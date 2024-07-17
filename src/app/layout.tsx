e8bf6c97059e1c6e497e09cea9ec2df5ab10a9d4
ribgu <gustavo.ribeiro@sptech.school>
2024-07-16 22:29:04 -0300

    remove chamada de componente

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import SideDrawer from './view/components/SideDrawer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +17,6 @@ export default function RootLayout({
   return (
     <html data-theme="light" lang="en">
       <body className={inter.className}>
-        <SideDrawer />
         {children}</body>
     </html>
   )
