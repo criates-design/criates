f46c4196d124d01c72af31010d7663ce995150e5
ribgu <gustavo.ribeiro@sptech.school>
2024-07-16 21:26:00 -0300

    add parte de baixo

--- src/app/view/components/SideDrawer.tsx
+++ src/app/view/components/SideDrawer.tsx
@@ -1,13 +1,27 @@
 export default function SideDrawer() {
-    return (
-        <div className="absolute h-full w-24 bg-criatesGray p-5">
-            <img src="logo.svg" />
-            <div className="flex bg-red-600 flex-col p-4">
-                <img src="dashboard-icon.svg" />
-                <img src="comment-icon.svg" />
-                <img src="download-icon.svg" />
-                <img src="edit-icon.svg" />
-            </div>
+  return (
+    <div className="absolute h-full w-24 bg-criatesGray p-5 py-8 flex flex-col justify-between">
+      <img src="logo.svg" />
+      <div className="flex justify-between h-5/6 flex-col">
+        <div className="flex flex-col p-4 gap-10">
+          <button className="btn">
+            <img src="dashboard-icon.svg" />
+          </button>
+          <button>
+            <img src="comment-icon.svg" />
+          </button>
+          <button>
+            <img src="download-icon.svg" />
+          </button>
+          <button>
+            <img src="edit-icon.svg" />
+          </button>
         </div>
-    )
+        <div className="flex flex-col p-4 gap-5">
+          <img src="perfil.svg" />
+          <img src="exit.svg" />
+        </div>
+      </div>
+    </div>
+  )
 }
