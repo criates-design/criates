9fff3d7803aabc548234b99acfde66ed31dc5f2e
ribgu <gustavo.ribeiro@sptech.school>
2024-07-24 20:54:03 -0300

    sideDrawer

--- src/view/components/SideDrawer.tsx
+++ src/view/components/SideDrawer.tsx
@@ -1,25 +1,37 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
 export default function SideDrawer() {
+  const router = useRouter()
+
+  const navigate = (path: string) => {
+    router.push(path)
+  }
   return (
     <div className="absolute h-full w-24 bg-criatesGray p-5 py-8 flex flex-col justify-between">
       <img src="logo.svg" />
       <div className="flex justify-between h-5/6 flex-col">
         <div className="flex flex-col p-4 gap-14">
-          <button>
+          <button onClick={() => navigate('/dashboard')} className='hover:opacity-50'>
             <img src="dashboard-icon.svg" />
           </button>
-          <button>
+          <button  className='hover:opacity-50'>
             <img src="edit-icon.svg" />
           </button>
-          <button>
+          <button onClick={() => navigate('/downloads')} className='hover:opacity-50'>
             <img src="download-icon.svg" />
           </button>
-          <button>
+          <button className='hover:opacity-50'>
             <img src="comment-icon.svg" />
           </button>
         </div>
         <div className="flex flex-col p-4 gap-10">
-          <img src="user.svg" />
-          <img src="exit.svg" />
+          <button onClick={() => navigate('/profile')} className='hover:opacity-50'>
+            <img src="user.svg" />
+          </button>
+          <button onClick={() => navigate('/downloads')} className='hover:opacity-50'>
+            <img src="exit.svg" />
+          </button>
         </div>
       </div>
     </div>
