027f19865070092a92e950167416574dc0078824
ribgu <gustavo.ribeiro@sptech.school>
2024-07-24 20:34:18 -0300

    move para src

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,10 @@
-import MyCreditsComponent from '../view/components/dashboard/MyCredits'
-import RequestComponent from '../view/components/dashboard/Request'
-import ArtStatusComponent from '../view/components/dashboard/ArtStatus'
-import AllArtsComponent from '../view/components/dashboard/AllArts'
-import DownloadsComponent from '../view/components/dashboard/Downloads'
-import UpgradePlanComponent from '../view/components/dashboard/UpgradePlan'
-import SideDrawer from '../view/components/SideDrawer'
+import MyCreditsComponent from '../../view/components/dashboard/MyCredits'
+import RequestComponent from '../../view/components/dashboard/Request'
+import ArtStatusComponent from '../../view/components/dashboard/ArtStatus'
+import AllArtsComponent from '../../view/components/dashboard/AllArts'
+import DownloadsComponent from '../../view/components/dashboard/Downloads'
+import UpgradePlanComponent from '../../view/components/dashboard/UpgradePlan'
+import SideDrawer from '../../view/components/SideDrawer'
 
 export default function Dashboard() {
     return (
