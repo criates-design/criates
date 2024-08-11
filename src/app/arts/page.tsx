07230a8f5ec596bae3cb98d83620ca97f149a3f1
Thiago <thiago.romaris@sptech.school>
2024-08-11 16:05:52 -0300

    chore: Update import path for ArtsProgress component in arts page

--- src/app/arts/page.tsx
+++ src/app/arts/page.tsx
@@ -1,5 +1,4 @@
-import ArtsProgress from '@/view/components/ArtsProgress/ArtsProgress'
-
+import ArtsProgress from '@/view/components/artsProgress/ArtsProgress'
 export default function AllArts() {
     return (
         <ArtsProgress />
